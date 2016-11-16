41b5a1d6e6c67139950de957a1269ac97e9ce067
Ahmed Soliman <ahmed.soliman@programmer.net>
2016-11-15 16:24:45 -0800

    coded nav bar

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Bootstrap 4</title>
+		<!-- Custom Google Fonts --> 
+		<!-- <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans+SC:400,700|BenchNine:300" rel="stylesheet"> -->
+		<!-- Font Awesome -->
+		<script src="https://use.fontawesome.com/2415393c0d.js"></script>
+		<!-- Animate.css -->
+		<link rel="stylesheet" type="text/css" href="css/animate.css">
+		<!-- Bootstrap.css -->
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+		<!-- Custom Style Sheet -->
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<!-- ******Jquery GOOGLE CDN***** -->
+		<script src="js/jquery-3.1.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/main.js"></script>
+	</head>
+	
+</html>
+
+<nav class="navbar navbar-default navbar-fixed-top">
+	<div class="container">
+		<a href="/bootstrap4/index.php" class="navbar-brand">Red Stone Shop</a>
+
+		<ul class="nav navbar-nav">
+			
+		</ul>
+	</div>
+</nav>
