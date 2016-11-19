a3ecd739b0e59024d963eb36fb48c6671894465d
Ahmed Soliman <ahmed.soliman@programmer.net>
2016-11-19 11:30:01 -0800

    debuged the typo of error

--- core/init.php
+++ core/init.php
@@ -1,8 +1,8 @@
 <?php 
 
-$db = mysqli_connect('127.0.0.1', 'root', '', 'redstoneshop');
-if (mysqli_connect_erroe()) {
-	echo "Database connecton faild with the following error: ".mysqli_connect_erroe();
+$db = mysqli_connect('127.0.0.1','root','','redstoneshop');
+if (mysqli_connect_error()) {
+	echo "Database connecton faild with the following error: ".mysqli_connect_error();
 	die();
 }
 ?>
