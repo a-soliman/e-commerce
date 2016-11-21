f6531901a423e9a4e759f6917a9665955ad87c04
Ahmed Soliman <ahmed.soliman@programmer.net>
2016-11-20 21:04:23 -0800

    removed the head part to be included in a new file for well orgnized code

--- index.php
+++ index.php
@@ -3,29 +3,7 @@
 	$sql = "SELECT * FROM products WHERE featured = 1";
 	$featured = $db->query($sql);
 	?>
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Bootstrap 4</title>
-		<!-- Custom Google Fonts --> 
-		<!-- <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans+SC:400,700|BenchNine:300" rel="stylesheet"> -->
-		<!-- Font Awesome -->
-		<script src="https://use.fontawesome.com/2415393c0d.js"></script>
-		<!-- Animate.css -->
-		<!-- <link rel="stylesheet" type="text/css" href="css/animate.css"> -->
-		<!-- Bootstrap.css -->
-		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-		<!-- Custom Style Sheet -->
-		<link rel="stylesheet" type="text/css" href="css/main.css">
-		<!-- ******Jquery GOOGLE CDN***** -->
-		<script src="js/jquery-3.1.1.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
-		<script src="js/main.js"></script>
-	</head>
 
-	<body>
 
 	<?php 
 	$sql = 'SELECT * FROM categories WHERE parent = 0';
@@ -59,11 +37,7 @@
 			</div>
 		</nav>
 
-		<!-- inserting images -->
-		<div id="background-image">
-			<div id="image-1"></div>
-			<div id="image-2"></div>
-		</div>
+		
 
 		<!-- Main Content -->
 		<div class="container-fluid featured-products">
