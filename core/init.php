66d17507e62b2ba93da2e7d4dea1ec6d2f4486e8
Ahmed Soliman <ahmed.soliman@programmer.net>
2016-11-22 12:02:21 -0800

    coded the session_start function

--- core/init.php
+++ core/init.php
@@ -5,4 +5,14 @@ if (mysqli_connect_error()) {
 	echo "Database connecton faild with the following error: ".mysqli_connect_error();
 	die();
 }
+
+session_start();
+require_once $_SERVER['DOCUMENT_ROOT'].'/config.php';
+require_once BASEURL. 'helpers/helpers.php';
+
+$cart_id = '';
+if (isset)($COOKIE[CART_COOKIE]) {
+	$cart_id = sanitize($_COOKIE[CART_COOKIE]);
+}
+
 ?>
