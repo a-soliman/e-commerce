cc6c5fb305c3e6fb134739961f1d25e0179afdac
Ahmed Soliman <ahmed.soliman@programmer.net>
2016-11-22 11:14:04 -0800

    linked to the new database 'shop'

--- core/init.php
+++ core/init.php
@@ -1,6 +1,6 @@
 <?php 
 
-$db = mysqli_connect('127.0.0.1','root','','redstoneshop');
+$db = mysqli_connect('127.0.0.1','root','','shop');
 if (mysqli_connect_error()) {
 	echo "Database connecton faild with the following error: ".mysqli_connect_error();
 	die();
