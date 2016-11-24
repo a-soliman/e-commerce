a4731831b6c7111b8765d8e1edc3e7b889ebf996
Ahmed Soliman <ahmed.soliman@programmer.net>
2016-11-24 10:21:35 -0800

    coded the configration started

---
+++ config.php
@@ -0,0 +1,9 @@
+<<?php 
+define ('BASEURL', $_SERVER['DOCUMENT_ROOT'].'/');
+define ('CART_COOKIE','SBwi72UCkLwiqzz2');
+define ('CART_COOKIE_EXPIRE', time() + (86400 * 30));
+define ('TAXRATE', 0.087); //sales tax rate. set to 0 if you aren't charging any tax
+
+define ('CURENCY', 'usd');
+define ('CHECKOUTMODE', 'TEST'); // Change "TEST" to "LIVE" when you're ready to go live
+
