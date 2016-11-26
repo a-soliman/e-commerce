e7827723cfc7bd1c4a397ef3c7f3c3bb0c0ebe87
Ahmed Soliman <ahmed.soliman@programmer.net>
2016-11-26 10:57:21 -0800

    DEBUGING and correcting Typos

--- cart.php
+++ cart.php
@@ -1,10 +1,10 @@
 <?php 
-require_once 'core/init.php'
-include 'includes/head.php'
-include 'includes/headerpartial.php'
+require_once 'core/init.php';
+include 'includes/head.php';
+include 'includes/headerpartial.php';
 
 if ($cart_id !='') {
-	$cartQ = db->query("SELECT * FROM cart WHILE id = '($cart_id)'");
+	$cartQ = $db->query("SELECT * FROM cart WHILE id = '($cart_id)'");
 	$result = mysqli_fetch_assoc($cartQ);
 	$items = json_decode($result['items'], true);
 	$i = 1;
