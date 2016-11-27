0f4dfc0c31c1939708b8a036a9b16279527c63ec
Ahmed Soliman <ahmed.soliman@programmer.net>
2016-11-26 16:39:35 -0800

    coded the functons for add to cart, update cart

--- includes/footer.php
+++ includes/footer.php
@@ -21,4 +21,52 @@
 			}
 		});
 	}
+
+	function update_cart(mode,edit_id,edit_size) {
+		var data = {"mode" : mode, "edit_id" : edit_id, "edit_size" : edit_size};
+		$.ajax ({
+			url ; '/admin/parsers/update_cart.php',
+			method : "post",
+			data : data,
+			success : function() {
+				location.reload();
+			},
+			error : function() {
+				alert("Something went wrong!");
+			}
+		})
+	}
+
+	function add_to_cart() {
+		$('#modal_errors').html("");
+		var size = $('#size').val();
+		var quantity = $('#quantity').val();
+		var available = $('#available').val();
+		
+		var error = '';
+		var data = $('#add_product_form').seralize();
+
+		if (size == '' || quantity == '' || quantity == 0) {
+			error += '<p class="text-danger text-center">You Must Choose a size and quantity</p>'
+			$('#modal_errors').html(error)
+			return;
+		} else if(quantity > available) {
+			error += '<p class="text-danger text-center">There are only' +available+ '</p>'
+			$('#modal_errors').html(error)
+			return;
+
+		} else {
+			$.ajax ({
+				url : '/admin/parsers/add_cart.php',
+				method : 'post',
+				data : data,
+				success : function() {
+					location.reload();
+				},
+				error: function() {
+					alert("Something went wrong!");
+				}
+			})
+		}
+	}
 </script>
