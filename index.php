8cfb254d5a211940a60e2b5fac9c8b5e792d2bf2
Ahmed Soliman <ahmed.soliman@programmer.net>
2016-11-28 20:44:01 -0800

    edited the index

--- index.php
+++ index.php
@@ -1,40 +1,41 @@
-<?php 
-	require_once 'core/init.php';
+<?php
+  require_once 'core/init.php';
+  include 'includes/head.php';
+  include 'includes/navigation.php';
+  include 'includes/headerfull.php';
 
-	include 'includes/head.php';
-	include 'includes/navigation.php';
-	include 'includes/headerfull.php';
-	$sql = "SELECT * FROM products WHERE featured = 1";
-	$featured = $db->query($sql);
-	?>
-
-	<!-- Main Content -->
-	<div class="container-fluid featured-products">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="row">
-				<h2 class="text-center">Featured Products</h2>
-				<?php while($product = mysqli_fetch_assoc($featured)) : ?>
-				<div class="col-sm-3">
-					<h4><?= $product['title']; ?></h4>
-					<img src="<?=$product['image']; ?>" alt="<?=$product['title']; ?>" id="images">
-					<p class="list-price text-danger">List Price: <s>$<?=$product['list_price']; ?></s></p>
-					<p class="price">Price: $<?=$product['price']; ?></p>
-					<button type="button" class="btn btn-success" onclick="detailsmodal(<?=$product['id']; ?>)">Details</button>
-				</div>
-				<?php endwhile; ?>
-
-			</div> <!-- row -->
-		</div> <!-- col-8 -->
-	</div>
+$sql = "SELECT * FROM products WHERE featured = 1";
+$featured = $db->query($sql);
+  ?>
 
 
-	<!-- Details Modul -->
-	<?php 
+<!-- Main Content -->
+  <div class="container-fluid featured-products">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="row">
+        <h2 class="text-center">Featured Products</h2>
+        <?php while($product = mysqli_fetch_assoc($featured)) : ?>
+        <div class="col-sm-3 text-center">
+          <h4><?= $product['title']; ?></h4>
+          <img src="<?=$product['image']; ?>" alt="<?=$product['title']; ?>" id="images">
+          <p class="list-price text-danger">List Price: <s>$<?=$product['list_price']; ?></s></p>
+          <p class="price">Price: $<?=$product['price']; ?></p>
+          <button type="button" class="btn btn-success" onclick="detailsmodal(<?=$product['id']; ?>)">Details</button>
+        </div>
+        <?php endwhile; ?>
 
-		  include 'includes/footer.php';
-	?>
-			
+      </div> <!-- row -->
+    </div> <!-- col-8 -->
+  </div>
 
 
-	</body>
+  <!-- Details Modul -->
+  <?php 
+
+      include 'includes/footer.php';
+  ?>
+      
+
+
+  </body>
 </html>
