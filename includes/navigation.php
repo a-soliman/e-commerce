b01f83d4173cedfd3d253f4df6315429626db144
Ahmed Soliman <ahmed.soliman@programmer.net>
2016-11-29 19:45:47 -0800

    changed the cart icon using bootstrap fonts

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,35 +1,32 @@
-<?php 
-	$sql = 'SELECT * FROM categories WHERE parent = 0';
-	$pquery = $db->query($sql);
-	?>
-	
-<!-- NaV -->
+<?php
+$sql = "SELECT * FROM categories WHERE parent = 0";
+$pquery = $db->query($sql);
+?>
+
 <nav class="navbar navbar-fixed-top">
-	<div class="container">
-		<a href="index.php" class="navbar-brand">Red Stone Shop</a>
-		
-		<!-- Dropdown menu -->
-		<ul class="nav navbar-nav">
-			<?php while($parent = mysqli_fetch_assoc($pquery)): ?>
-			<?php 
-			$parent_id = $parent ['id'];
-			$sql2 = "SELECT * FROM categories WHERE parent = '$parent_id'";
-			$cquery = $db->query($sql2);
-			?>
-			<li class="dropdown">
-				<a href="#" class="dropdown-tggle" data-toggle="dropdown" id="text"><?php echo $parent['category']; ?><span class="caret"></span></a>
-				<ul class="dropdown-menu" role="menu">
-					<?php while($child = mysqli_fetch_assoc($cquery)) :?>
-					<li>
-						<a href="#"><?php echo $child['category']; ?></a>
-					</li>
-					<?php endwhile; ?>
-				</ul>
-			</li>
-			<?php endwhile; ?>
-		</ul>
-		<ul class="nav navbar-nav cart">
-			<li><a href="cart.php" id="cartbutton"><span> <i class="fa fa-shopping-cart" aria-hidden="true"></i></span> My Cart</a></li>
-		</ul>
-	</div>
-</nav>
+   <div class="container">
+     <a href="/tutorial/index.php" class="navbar-brand" id="text">Red Stone Shop</a>
+     <ul class="nav navbar-nav" id="text">
+       <?php while($parent = mysqli_fetch_assoc($pquery)) : ?>
+         <?php
+          $parent_id = $parent['id'];
+          $sql2 = "SELECT * FROM categories WHERE parent = '$parent_id'";
+          $cquery = $db->query($sql2);
+         ?>
+
+         <!-- Menu Items -->
+         <li class="dropdown" id="text">
+             <a href="#" class="dropdown-toggle" data-toggle="dropdown" id="text"><?php echo $parent['category']; ?><span class="caret" id="text"></span></a>
+             <ul class="dropdown-menu" role="menu" id="text">
+               <?php while($child = mysqli_fetch_assoc($cquery)) : ?>
+                 <li><a href="category.php?cat=<?=$child['id'];?>"><?php echo $child['category']; ?></a></li>
+               <?php endwhile; ?>
+             </ul>
+         </li>
+       <?php endwhile; ?>
+     </ul>
+     <ul class="nav navbar-nav cart">
+      <li><a href="cart.php" id="text"><span class="glyphicon glyphicon-shopping-cart"></span> My Cart</a></li>
+    </ul>
+   </div>
+ </nav>
