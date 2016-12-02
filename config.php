9abb8e1392a5511f9cbe27e9077c9709a15a8a68
Ahmed Soliman <ahmed.soliman@programmer.net>
2016-12-01 19:57:32 -0800

    added my keys to the stripe account

--- config.php
+++ config.php
@@ -1,19 +1,18 @@
-<<?php 
-define ('BASEURL', $_SERVER['DOCUMENT_ROOT'].'/');
-define ('CART_COOKIE','SBwi72UCkLwiqzz2');
-define ('CART_COOKIE_EXPIRE', time() + (86400 * 30));
-define ('TAXRATE', 0.087); //sales tax rate. set to 0 if you aren't charging any tax
+<?php
+define('BASEURL', $_SERVER['DOCUMENT_ROOT'].'/tutorial/');
+define('CART_COOKIE','SBwi72UCklwiqzz2');
+define('CART_COOKIE_EXPIRE',time() + (86400 *30));
+define('TAXRATE',0.087); //Sales tax rate. Set to 0 if you are arn't charging tax.
 
-define ('CURENCY', 'usd');
-define ('CHECKOUTMODE', 'TEST'); // Change "TEST" to "LIVE" when you're ready to go live
+define('CURRENCY', 'usd');
+define('CHECKOUTMODE','TEST'); //Change TEST to LIVE when you are ready to go LIVE
 
-if(CHECKOUTMODE == 'TEST') {
-	define('STRIPE_PRIVATE','insertYourOwnStripeKey');
-	define('STRIPE_PUBLIC','insertYourOwnStripeKey');
+if(CHECKOUTMODE == 'TEST'){
+  define('STRIPE_PRIVATE','sk_test_n9PH4EiHTomoRhS5xu08lDCU');
+  define('STRIPE_PUBLIC', 'pk_test_jWgX9f7MyxLlP36ZwiPyfPtC');
 }
 
-if(CHECKOUTMODE == 'LIVE') {
-	define('STRIPE_PRIVATE','insertYourOwnStripeKey');
-	define('STRIPE_PUBLIC','insertYourOwnStripeKey');
+if(CHECKOUTMODE == 'LIVE'){
+  define('STRIPE_PRIVATE','insertyourownstripekey');
+  define('STRIPE_PUBLIC', 'insertyourownstripekey');
 }
- ?>
